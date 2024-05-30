bf7f3af99bde5f4d53d79cbba2c7edf814fa069e
wrouruy <161200383+wrouruy@users.noreply.github.com>
2024-05-30 20:03:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+onGithub.onclick = function(){
+    window.location.href = 'https://github.com/wrouruy';
+}
+
+// dot reaction (First)
+viewGame.onclick = function(){
+    window.location.href = 'https://wrouruy.github.io/dotReaction/';
+}
+viewGithub.onclick = function(){
+    window.location.href = 'https://github.com/wrouruy/dotReaction';
+}
+
+// ... (Second)
+viewGameOne.onclick = function(){
+    window.location.href = 'https://wrouruy.github.io/file-Detector/';
+}
+viewGithubOne.onclick = function(){
+    window.location.href = 'https://github.com/wrouruy/file-Detector';
+}
+
+// ... (Thirt)
+viewGameTwo.onclick = function(){
+    window.location.href = '';
+}
+viewGithubTwo.onclick = function(){
+    window.location.href = '';
+}
+
+// ... (Four)
+viewGameThree.onclick = function(){
+    window.location.href = '';
+}
+viewGithubThree.onclick = function(){
+    window.location.href = '';
+}
